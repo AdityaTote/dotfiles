50d4200b7f45849a7739a905fcbaf8a492e87f6a
AdityaTote <adityatote23@gmail.com>
2025-06-22 02:18:46 +0530

    init: dotfiles

---
+++ .config/zsh/alias.sh
@@ -0,0 +1,60 @@
+#-----------------------------------------------------------------------------
+# git
+#-----------------------------------------------------------------------------
+alias gint='git init'
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit -m'
+alias gac='gaa && gc'
+alias gps='git push'
+alias gpsh='gp origin HEAD'
+alias gpsd='gp origin dev'
+alias gpsm='gp origin main'
+alias gb='git branch'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gpl='git pull origin'
+alias gpld='gpl dev'
+alias gr='git remote origin'
+
+#-----------------------------------------------------------------------------
+# npm
+#----------------------------------------------------------------------------
+alias nint='npm init --y'
+alias ni='npm install'
+alias nd='npm run dev'
+alias nb='npm run build'
+alias ns='npm run start'
+
+#-----------------------------------------------------------------------------
+# pnpm
+#----------------------------------------------------------------------------
+alias pnit='pnpm init'
+alias pna='pnpm add'
+alias pni='pnpm install'
+alias pnd='pnpm dev'
+alias pnb='pnpm build'
+alias pns='pnpm start'
+
+#-----------------------------------------------------------------------------
+# bun
+#----------------------------------------------------------------------------
+alias bnit='bun init'
+alias bna='bun add'
+alias bni='bun install'
+alias bnd='bun dev'
+alias bnb='bun build'
+alias bns='bun start'
+
+#-----------------------------------------------------------------------------
+# imp dir to access
+#----------------------------------------------------------------------------
+WORKDIR="$HOME/work/code"
+
+alias cdw="cd $WORKDIR"
+alias cdi="cd $WORKDIR/intern"
+alias cdiq="cd $WORKDIR/intern/quick-hire"
+alias cdp="cd $WORKDIR/projects"
+alias cdp2="cd $WORKDIR/projects/web-2"
+alias cdch="cd $WORKDIR/cohert"
+
